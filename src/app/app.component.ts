6d01edbd9358b10ce702c24abd54ec3ff2497c6e
rakesh@cloud <rakesh.r@cloudphysician.net>
2024-07-08 10:40:02 +0530

    buttons add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-cookies';
+  constructor(private http: HttpClient) {}
+
+  sendRequest1() {
+    this.http.get('https://api.example.com/endpoint1').subscribe(response => {
+      console.log('Response from endpoint1:', response);
+    }, error => {
+      console.error('Error from endpoint1:', error);
+    });
+  }
+
+  sendRequest2() {
+    this.http.get('https://api.example.com/endpoint2').subscribe(response => {
+      console.log('Response from endpoint2:', response);
+    }, error => {
+      console.error('Error from endpoint2:', error);
+    });
+  }
 }
